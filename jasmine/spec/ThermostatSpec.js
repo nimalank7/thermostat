73b3575b23df6baf6af2b19bb58e97c66237762a
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2018-11-27 14:37:00 +0000

    sets power saving property and doesn't increment past 25 if on

--- jasmine/spec/ThermostatSpec.js
+++ jasmine/spec/ThermostatSpec.js
@@ -1,10 +1,34 @@
 describe ("Thermostat", function() {
+  var thermostat;
 
   beforeEach(function(){
-
+    thermostat = new Thermostat();
   });
-  describe('sets up capacity to empty array', function (){
-    it('responds true', function() {
+  describe('sets up a thermostat object', function (){
+    it('initializes temperature to 20', function() {
+      expect(thermostat.temperature).toEqual(20);
+    });
+    it('increases the temperature by user given value', function() {
+      thermostat.up(5);
+      expect(thermostat.temperature).toEqual(25);
+    });
+    it('decreases the temperature by user given value', function() {
+      thermostat.down(10);
+      expect(thermostat.temperature).toEqual(10);
+    });
+    it('returns false when attempting to decrement by 10', function() {
+      expect(thermostat.down(11)).toEqual(false);
+    });
+    it('temperature remains at 20 when decrementing more than minimum value', function() {
+      thermostat.down(11);
+      expect(thermostat.temperature).toEqual(20);
+    });
+    it('sets power saving mode off for false', function() {
+      expect(thermostat.isPowerSaving).toEqual(false);
+    });
+    it('doesnt increment past 25 if power saving mode is on', function() {
+      thermostat.isPowerSaving = true;
+      expect(thermostat.up(6)).toEqual(false);
     });
   });
 });
--- jasmine/src/thermostat.js
+++ jasmine/src/thermostat.js
@@ -1,1 +1,16 @@
-
+function Thermostat() {
+  this.temperature = 20;
+  this.isPowerSaving = false;
+  this.up = function (increment) {
+    if(this.temperature + increment > 25){
+        return false;
+      }
+      this.temperature += increment;
+  };
+  this.down = function (decrement) {
+      if(this.temperature - decrement < 10){
+        return false;
+      }
+      this.temperature -= decrement;
+  };
+}
